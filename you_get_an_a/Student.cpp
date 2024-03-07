b23cc1fd5fba786feea1035c466c0e9f2a0b0d65
LeoG123321 <157080843+LeoG123321@users.noreply.github.com>
2024-03-06 19:36:46 -0600

    declared functions for class "Student"

--- you_get_an_a/Student.cpp
+++ you_get_an_a/Student.cpp
@@ -1,1 +1,32 @@
 #include "Student.h"
+
+Student::Student(string first_name, string last_name, string student_id) {
+	this->first_name = first_name;
+	this->last_name = last_name;
+	this->student_id = student_id;
+}
+
+string Student::get_first_name() {
+	return first_name;
+}
+
+string Student::get_last_name() {
+	return last_name;
+}
+
+string Student::get_student_id() {
+	return student_id;
+}
+
+void Student::create_assignment(string assignment_name) {
+	Assignment place_holder(assignment_name, 0);
+	student_assignment.push_back(place_holder);
+}
+
+void Student::grade_assignment(string assignment_name, int points) {
+	for (int i = 0; i != student_assignment.size(); i++) {
+		if (student_assignment[i].get_assignment_name() == assignment_name) {
+			student_assignment[i].set_points(points);
+		}
+	}
+}
