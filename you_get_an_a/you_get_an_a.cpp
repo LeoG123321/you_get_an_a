7ea6cea7cccdfc3cc2d4fa0ea6761a8ad7957116
LeoG123321 <157080843+LeoG123321@users.noreply.github.com>
2024-03-06 18:47:28 -0600

    Add project files.

---
+++ you_get_an_a/you_get_an_a.cpp
@@ -0,0 +1,14 @@
+/*
+    Leonardo Gonzalez   3/6/2024
+
+    Week 7              You Get an A!
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
